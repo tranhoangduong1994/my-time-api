22f157fcabea4ec83f77e89970eaedb20e689ba3
Tran Hoang Duong <tranhoangduong1994@gmail.com>
2016-07-19 09:08:06 +0000

    support cross domain request

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/", function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.json({
     'timestamp': new Date().getTime() / 1000
   });
