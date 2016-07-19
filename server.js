70780623be045601b7fa563bb2f72effa7f60b15
Tran Hoang Duong <tranhoangduong1994@gmail.com>
2016-07-19 10:20:25 +0000

    get rid of package cors

--- server.js
+++ server.js
@@ -5,22 +5,27 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:8000");
-//   res.header("Access-Control-Allow-Origin", "https://apps.facebook.com/classic-vegas-slots");
-//   res.header("Access-Control-Allow-Origin", "https://hub102.com/staging/slots");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-var whitelist = ['http://localhost:8000', 'https://apps.facebook.com/classic-vegas-slots', 'https://hub102.com/staging/slots'];
-var corsOptions = {
-  origin: function(origin, callback){
-    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-    callback(null, originIsWhitelisted);
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  // res.header("Access-Control-Allow-Origin", "https://apps.facebook.com/classic-vegas-slots");
+  // res.header("Access-Control-Allow-Origin", "https://hub102.com/staging/slots");
+  var allowedOrigins = ["http://localhost:8000", "https://apps.facebook.com/classic-vegas-slots", "https://hub102.com/staging/slots"];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
   }
-};
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+// var whitelist = ['http://localhost:8000', 'https://apps.facebook.com/classic-vegas-slots', 'https://hub102.com/staging/slots'];
+// var corsOptions = {
+//   origin: function(origin, callback){
+//     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+//     callback(null, originIsWhitelisted);
+//   }
+// };
 
-app.get("/", cors(corsOptions), function(req, res) {
+app.get("/", function(req, res) {
   res.json({
     'timestamp': new Date().getTime() / 1000
   });
